39a81e560f9bb2526c616258641d7a6f587168a1
ENSLERMAN <marsel.garipov.rus@gmail.com>
2019-11-10 01:03:51 +0300

    add preloader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'honor';
+export class AppComponent implements OnInit, AfterViewInit{
+
+  constructor(private spinner: NgxSpinnerService) { }
+
+  ngOnInit() {
+    this.spinner.show();
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 2000)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { NgxSpinnerModule } from "ngx-spinner";
 
 
 @NgModule({
@@ -28,7 +29,8 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatIconModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    NgxSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
