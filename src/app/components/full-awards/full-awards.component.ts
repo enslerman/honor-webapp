2f781389e3921b3b199f8e5a2004c00908258fb1
ENSLERMAN <marsel.garipov.rus@gmail.com>
2019-11-17 22:42:25 +0300

    add full awards

--- src/app/components/full-awards/full-awards.component.ts
+++ src/app/components/full-awards/full-awards.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/services/http.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-full-awards',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FullAwardsComponent implements OnInit {
 
-  constructor() { }
+  constructor(  
+    private API: HttpService, 
+    private activatedRouter: ActivatedRoute,
+  ) { 
+    this.activatedRouter.params.subscribe(param => {
+      this.id = param.id;
+    });
+  }
+
+  id: number;
+  award:any = {
+  };
+  veterans: any;
+
+  async getAward(){
+    this.award = await this.API.getAwardById(this.id);
+    this.veterans = this.award.veterans;
+  }
 
   ngOnInit() {
+    this.getAward().then(()=>{
+      console.log(this.award);
+      console.log(this.veterans)
+    })
   }
 
 }
