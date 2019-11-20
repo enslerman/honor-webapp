feb9db9be8be84b5f0f6293fe24eb6fb777ec73c
treloiii <nikkraew22@gmail.com>
2019-11-20 23:15:31 +0300

    try to make comments

--- src/app/components/full-gallery/full-gallery.component.ts
+++ src/app/components/full-gallery/full-gallery.component.ts
@@ -45,7 +45,7 @@ export class FullGalleryComponent implements OnInit {
 
   openDialog(id): void {
     let dialogRef = this.dialog.open(ImageModalComponent, {
-      width: "700px",
+      width: "900px",
       //maxHeight: "",
       data: {
         id:id, 
