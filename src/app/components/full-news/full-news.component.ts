3d0b3be99698f486538f8bd023a4cd4f20ee2779
treloiii <nikkraew22@gmail.com>
2019-12-10 01:14:53 +0300

    refactor urls

--- src/app/components/full-news/full-news.component.ts
+++ src/app/components/full-news/full-news.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/services/http.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-full-news',
@@ -13,14 +14,15 @@ export class FullNewsComponent implements OnInit {
   constructor(  
     private API: HttpService, 
     private activatedRouter: ActivatedRoute,
-    private location:Location
+    private location:Location,
+    private sanitizer: DomSanitizer
   ) { 
     this.activatedRouter.params.subscribe(param => {
       console.log(param);
       this.id = param.id;
     });
   }
-
+  htmlData;
   id: number;
   news:any = {
     "author": "",
@@ -37,6 +39,7 @@ export class FullNewsComponent implements OnInit {
 
   ngOnInit() {
     this.getNews().then(()=>{
+      this.htmlData=this.sanitizer.bypassSecurityTrustHtml(this.news.description);
       console.log(this.news);
     })
   }
