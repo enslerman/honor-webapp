eba907fefd5d68d3405da11c2d3ff60ab3434859
Marsel Garipov <marsel.garipov.rus@gmail.com>
2019-12-13 19:18:54 +0300

    ggwpp

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpService } from 'src/app/services/http.service';
 import { useAnimation, transition,trigger,query,style,animate } from "@angular/animations";
@@ -22,15 +22,15 @@ export interface Tile {
 export class MainComponent implements OnInit {
 
   constructor(private http: HttpClient, private API: HttpService) {}
-
-  lasts:any[]=[];
+  public innerWidth: any;
+  //lasts:any[]=[];
   slides:any = [
     {
       image:""
     }
   ];
   news: any = [];
-  vh = window.innerHeight * 0.01;
+  vh = window.innerHeight;
   tiles: Tile[] = [
     {text: 'One', cols: 2, rows: 1, color: 'blue'},
     {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
@@ -46,6 +46,36 @@ export class MainComponent implements OnInit {
     "image":"",
     hdn:false
   };
+  lasts: any[] = [
+    {
+      "image": "https://9net.ru/uploads/posts/2019-01/1546515622_ricardo.gif",
+    },
+    {
+      "image": "https://steamuserimages-a.akamaihd.net/ugc/951841177148216940/4C0E46016EE7ABF8440FCA7B9B5AB60EF55AA969/?imw=512&imh=384&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true",
+    },
+    {
+      "image": "https://steamuserimages-a.akamaihd.net/ugc/956346433295880717/87FE6FB71818889F98F79BDA6BFC22BF99EA4112/",
+    },
+    {
+      "image": "https://66.media.tumblr.com/366ad4b76e87b2787aecdcd8842b3f34/tumblr_p4h0p25SAo1wyxz4yo1_500.gifv",
+    },
+    {
+      "image": "https://steamuserimages-a.akamaihd.net/ugc/951841177148216940/4C0E46016EE7ABF8440FCA7B9B5AB60EF55AA969/?imw=512&imh=384&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true",
+    }
+  ]
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+  this.innerWidth = window.innerWidth;
+  console.log(this.innerWidth);
+  console.log(this.vh);
+
+  if(this.vh>this.innerWidth){
+    for(let tile of this.tiles)
+      tile.cols=3;
+  }
+    
+  } 
 
   async getPosts(){
     this.slides = await this.API.getMain();
@@ -73,10 +103,10 @@ export class MainComponent implements OnInit {
     this.getNews().then(()=> {
       console.log(this.news)
     });
-    this.getLasts().then(()=> {
-      console.log(this.lasts)
+    // this.getLasts().then(()=> {
+    //   console.log(this.lasts)
       this.lasts.unshift({})
-    });
+    // });
     this.getLastPhotos().then(()=> {
       console.log(this.photos);
     });
