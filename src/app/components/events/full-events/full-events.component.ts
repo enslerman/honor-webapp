e3e11bed8264d19ed04821372b6bef8a65c424a4
Marsel Garipov <marsel.garipov.rus@gmail.com>
2019-12-20 00:45:22 +0300

    update full-pages

--- src/app/components/events/full-events/full-events.component.ts
+++ src/app/components/events/full-events/full-events.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/services/http.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-full-events',
@@ -13,13 +14,17 @@ export class FullEventsComponent implements OnInit {
   constructor(  
     private API: HttpService, 
     private activatedRouter: ActivatedRoute,
-    private location:Location
+    private location:Location,
+    private sanitizer: DomSanitizer
   ) { 
     this.activatedRouter.params.subscribe(param => {
       this.id = param.id;
     });
   }
 
+  OtherEvents: any = [{}];
+
+  htmlData;
   id: number;
   event:any = {
     author: "",
@@ -34,13 +39,21 @@ export class FullEventsComponent implements OnInit {
     }
   };
 
+  async getOtherEvents() {
+    this.OtherEvents = await this.API.getEvents();
+  }
+
   async getEvent(){
     this.event = await this.API.getEventById(this.id);
   }
 
   ngOnInit() {
     this.getEvent().then(()=>{
+      this.htmlData = this.sanitizer.bypassSecurityTrustHtml(this.event.description);
       console.log(this.event);
+    })
+    this.getOtherEvents().then(async () => {
+      console.log(this.OtherEvents)
     })
   }
 
